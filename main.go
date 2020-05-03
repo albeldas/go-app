042d3cc0dcdd05e9013bd986a9256468c1f78ab2
yakireliyahu1987 <38716819+yakireliyahu1987@users.noreply.github.com>
2020-05-03 10:48:36 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 // Sample app
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Allcloud DOE Demo app canary develop v1.4.2"
+	title := "Allcloud DOE Demo app canary develop v1.4.3"
 	from := ""
 	if r.URL != nil {
 		from = r.URL.String()
